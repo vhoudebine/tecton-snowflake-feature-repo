b14d678f5bf21650ec2eed38023b596e7c17bef2
Vincent Houdebine <vincenthoudebine@vincenthoudebine-mba.mynetworksettings.com>
2023-07-18 21:11:14 -0400

    Updated tutorial notebooks with NDD

--- feature_repo/features/batch_feature_views/user_last_transaction_amount.py
+++ feature_repo/features/batch_feature_views/user_last_transaction_amount.py
@@ -4,22 +4,22 @@ from data_sources.transactions import transactions
 from datetime import datetime, timedelta
 
 
-# @batch_feature_view(
-#     sources=[transactions],
-#     entities=[user],
-#     mode='snowflake_sql',
-#     online=True,
-#     batch_schedule=timedelta(days=1),
-#     ttl=timedelta(days=30),
-#     feature_start_time=datetime(2021, 5, 20),
-#     description='Last user transaction amount (batch calculated)'
-# )
-# def user_last_transaction_amount(transactions):
-#     return f'''
-#         SELECT
-#             USER_ID,
-#             AMT,
-#             TIMESTAMP
-#         FROM
-#             {transactions}
-#         '''
+@batch_feature_view(
+    sources=[transactions],
+    entities=[user],
+    mode='snowflake_sql',
+    online=True,
+    batch_schedule=timedelta(days=1),
+    ttl=timedelta(days=30),
+    feature_start_time=datetime(2021, 5, 20),
+    description='Last user transaction amount (batch calculated)'
+)
+def user_last_transaction_amount(transactions):
+    return f'''
+        SELECT
+            USER_ID,
+            AMT,
+            TIMESTAMP
+        FROM
+            {transactions}
+        '''
